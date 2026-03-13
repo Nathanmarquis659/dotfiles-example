fd00557cd5b89b3ee6c1977c6926382791a07e4f
NathanMarquis659 <nathanmarquis659@onyx.boisestate.edu>
2026-03-13 17:46:17 -0600

    Fixed bash_settings.sh bug line 16; added some shortcuts for git

--- bash_settings.sh
+++ bash_settings.sh
@@ -13,7 +13,9 @@ append_path "$HOME/.local/bin"
 
 #Set the prompt for both ZSH and BASH
 if [ -n "$ZSH_VERSION" ]; then
-    precmd () { __git_ps1 "%n" ":%~$ " "|%s" }
+    precmd () {
+	    __git_ps1 "%n" ":%~$ " "|%s"
+    }
 elif [ -n "$BASH_VERSION" ]; then
     PROMPT_COMMAND='__git_ps1 "\u@\h:\w" "\\\$ "'
 fi
@@ -29,3 +31,31 @@ GIT_PS1_SHOWUNTRACKEDFILES=true
 #Add in alias
 alias ll='ls -l'
 alias la='ls -l -a'
+
+# ========================================
+# Aliases for frequently used git commands
+# ========================================
+alias gits='git status'
+alias gw='git show'
+alias gwc='git show --color-words'
+alias gws='git show --stat'
+alias ga='git add .'
+alias gd='git diff'
+alias gds='git diff --staged'
+alias gdw='git diff --color-words'
+alias gg='git customLog1'
+alias gg2='git customLog2'
+alias gg3='git customLog3'
+alias gg4='git customLog4'
+alias ggb='git log --oneline --decorate --graph --all'
+alias ggba='git log --oneline --decorate --graph --all --author-date-order'
+alias gk='gitk --all'
+alias gt='git mergetool'
+alias gco='git checkout'
+alias grc='git rebase --continue'
+alias grv='git remote -v'
+alias gba='git branch -avv'
+alias gbv='git branch -vv'
+alias grpo='git remote prune origin --dry-run'
+alias nopush='git remote set-url --push origin no_push_url' #useful when exploring a repo for which you have write access to, but don't want to accidentaly push any changes
+
